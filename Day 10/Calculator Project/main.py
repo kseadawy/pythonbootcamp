b7443da92e62e8fe958e219c3fbecc829a451f7c
KHALID ELSEADAWY <kseadawy@gmail.com>
2025-08-02 22:49:32 -0500

    Day 10 commited

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,32 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+keys = {"+": add, "-": subtract, "*": multiply, "/": divide}
+res = 0
+again = 'n'
+while True:
+    if  again == 'n':
+        num1 = float(input("What's the first number?:  "))
+    else:
+        num1 = res
+    for key in keys:
+        print(key)
+    opr = input("Pick an operation: ")
+
+    num2 = float(input("What's the next number?:  "))
+
+    res = keys[opr](num1, num2)
+    print(f"{num1} {opr} {num2} = {res}")
+
+    again = input(f"Type 'y' to continue with {res}, type 'n' to start new calculation:")
 
---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+    full_name = f_name + " "+ l_name
+    return  full_name.title()
+
+my_name = format_name("khalid", "elseadawy")
+print(my_name)
