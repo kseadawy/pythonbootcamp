0eaac397220a458a94dcc0cae84f282d8dc458b1
KHALID ELSEADAWY <kseadawy@gmail.com>
2025-08-06 23:39:59 -0500

    Day 13 commited

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+#It iterates from num 1 to 19 to implement code inside it
 # 2. When is the function meant to print "You got it"?
+#When range is changed to 21 instead of 20
 # 3. What are your assumptions about the value of i?
+#change end of range to be 21 instead of 20
